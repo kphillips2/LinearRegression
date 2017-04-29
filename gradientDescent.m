4d78a7ae5a30edb584982ced8fb782405d6a9c21
kphillips2 <kephillips@comcast.net>
2017-04-29 16:08:01 -0700

    Revert "Fixed m value"
    This reverts commit 52923eb60b715fe8a9818931a4fda45d59e0fb1b.

--- gradientDescent.m
+++ gradientDescent.m
@@ -5,7 +5,9 @@
 
 function[theta] = gradientDescent(X, Y, theta, alpha, num_iterations)
 
+m = length(Y);
+
 for i = 1:num_iterations
-    slope = (alpha/num_iterations) * (X * theta - Y)' * X(:,2); 
+    slope = (alpha/m) * (X * theta - Y)' * X(:,2); 
     theta = theta - slope;
 end
