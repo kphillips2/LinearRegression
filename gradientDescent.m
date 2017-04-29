52923eb60b715fe8a9818931a4fda45d59e0fb1b
kphillips2 <kephillips@comcast.net>
2017-04-29 16:05:30 -0700

    Fixed m value

--- gradientDescent.m
+++ gradientDescent.m
@@ -5,9 +5,7 @@
 
 function[theta] = gradientDescent(X, Y, theta, alpha, num_iterations)
 
-m = length(Y);
-
 for i = 1:num_iterations
-    slope = (alpha/m) * (X * theta - Y)' * X(:,2); 
+    slope = (alpha/num_iterations) * (X * theta - Y)' * X(:,2); 
     theta = theta - slope;
 end
