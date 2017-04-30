b1495cd56f1119648268dd41597621e2dfc37fda
kphillips2 <kephillips@comcast.net>
2017-04-30 09:26:54 -0700

    Changed so two slopes are calculated

--- gradientDescent.m
+++ gradientDescent.m
@@ -8,6 +8,8 @@ function[theta] = gradientDescent(X, Y, theta, alpha, num_iterations)
 m = length(Y);
 
 for i = 1:num_iterations
-    slope = (alpha/m) * (X * theta - Y)' * X(:,2); 
-    theta = theta - slope;
+    slope0 = (alpha/m) * (X * theta - Y)' * X(:,1);
+    slope1 = (alpha/m) * (X * theta - Y)' * X(:,2); 
+    theta(1) = theta(1) - slope0;
+    theta(2) = theta(2) - slope1;
 end
